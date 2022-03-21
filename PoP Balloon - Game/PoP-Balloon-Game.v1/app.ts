367859c971354185dd567ce1d1b92b1cda36eed9
GiliEllen <gilimena95@gmail.com>
2022-03-21 17:58:27 +0200

    vhh

--- PoP Balloon - Game/PoP-Balloon-Game.v1/app.ts
+++ PoP Balloon - Game/PoP-Balloon-Game.v1/app.ts
@@ -2,6 +2,12 @@ console.log(`hi!`)
 const body = document.querySelector("body");
 const balloonHolderArr = [];
 
+// Function:
+// In interval, creates div element
+// Appends div to body
+// Add img to div, and change the source
+// Push baloon-holder to an array
+// Runs the function designBalloon
 function createBalloon() {
     const balloonHolder: HTMLDivElement = document.createElement('div');
     body.append(balloonHolder);
@@ -11,33 +17,58 @@ function createBalloon() {
     balloonImg.src = "Assets/images/balloon.png"
     moveBalloon(balloonHolder)
     balloonHolderArr.push(balloonHolder)
+    return balloonHolderArr
 }
 
+// Function:
+// Assigns balloon-holder a class
+// Defines startinf top and left position using a random X number
 function designBalloon(balloonHolder: HTMLDivElement) {
     balloonHolder.classList.add('balloon-holder');
     balloonHolder.style.left = `${getRandomNumber(10, 90)}vw`
     balloonHolder.style.top = `112vh`
 }
 
+// Function:
+// Gets to parametrs: minmum and maximum numbers
+// Gets a random number between two numbers
 function getRandomNumber(min: number, max: number) {
     return Math.floor(Math.random() * (max - min) + min)
 }
 
+// Function:
+// Changes top of baloon-holder after 0.5 sec
 function moveBalloon(balloonHolder) {
     setTimeout(() => {
-        balloonHolder.style.top = "-15vh"
+        balloonHolder.style.top = "-30vh"
     }, 500);
 }
 
+// Function:
+// For each element in the array gets top position
+// if balloon reach top run function removeBalloon
 function checkAndRemove() {
-    balloonHolderArr.forEach(balloonHolder => {
-        removeBalloon(balloonHolder)
+    balloonHolderArr.forEach(elm => {
+        if (elm.getBoundingClientRect().top <= -370) {
+            removeBalloon(elm)
+        }
     });
 }
+
+// Function:
+// Gets balloon and removes
 function removeBalloon(balloonHolder) {
-    if (parseFloat(balloonHolder.style.left) === -15) {
-        balloonHolder.remove()
-    }
+    balloonHolder.remove()
 }
-setInterval(checkAndRemove,100)
+
+function checkIfClick() {
+    balloonHolderArr.forEach(element => {
+        element.addEventListener('click', ev => {
+            element.remove()
+        })
+    })
+}
+
+setInterval(checkAndRemove, 100)
 setInterval(createBalloon, getRandomNumber(2500, 5000))
+setInterval(checkIfClick, 10)
--- PoP Balloon - Game/PoP-Balloon-Game.v1/dist/app.js
+++ PoP Balloon - Game/PoP-Balloon-Game.v1/dist/app.js
@@ -1,6 +1,12 @@
 console.log("hi!");
 var body = document.querySelector("body");
 var balloonHolderArr = [];
+// Function:
+// In interval, creates div element
+// Appends div to body
+// Add img to div, and change the source
+// Push baloon-holder to an array
+// Runs the function designBalloon
 function createBalloon() {
     var balloonHolder = document.createElement('div');
     body.append(balloonHolder);
@@ -10,29 +16,51 @@ function createBalloon() {
     balloonImg.src = "Assets/images/balloon.png";
     moveBalloon(balloonHolder);
     balloonHolderArr.push(balloonHolder);
+    return balloonHolderArr;
 }
+// Function:
+// Assigns balloon-holder a class
+// Defines startinf top and left position using a random X number
 function designBalloon(balloonHolder) {
     balloonHolder.classList.add('balloon-holder');
     balloonHolder.style.left = getRandomNumber(10, 90) + "vw";
     balloonHolder.style.top = "112vh";
 }
+// Function:
+// Gets to parametrs: minmum and maximum numbers
+// Gets a random number between two numbers
 function getRandomNumber(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
 }
+// Function:
+// Changes top of baloon-holder after 0.5 sec
 function moveBalloon(balloonHolder) {
     setTimeout(function () {
-        balloonHolder.style.top = "-15vh";
+        balloonHolder.style.top = "-30vh";
     }, 500);
 }
+// Function:
+// For each element in the array gets top position
+// if balloon reach top run function removeBalloon
 function checkAndRemove() {
-    balloonHolderArr.forEach(function (balloonHolder) {
-        removeBalloon(balloonHolder);
+    balloonHolderArr.forEach(function (elm) {
+        if (elm.getBoundingClientRect().top <= -370) {
+            removeBalloon(elm);
+        }
     });
 }
+// Function:
+// Gets balloon and removes
 function removeBalloon(balloonHolder) {
-    if (parseFloat(balloonHolder.style.left) === -15) {
-        balloonHolder.remove();
-    }
+    balloonHolder.remove();
+}
+function checkIfClick() {
+    balloonHolderArr.forEach(function (element) {
+        element.addEventListener('click', function (ev) {
+            element.remove();
+        });
+    });
 }
 setInterval(checkAndRemove, 100);
 setInterval(createBalloon, getRandomNumber(2500, 5000));
+setInterval(checkIfClick, 10);
